451526f7a389a9855b669e121a0ab1a3d452eee5
jaeheoon <aleh101@naver.com>
2023-09-12 15:41:14 +0900

    OpenAPI 이용해서 리스트 불러오기

--- workspace/src/godDog_DB/GodDog_dumi.sql
+++ workspace/src/godDog_DB/GodDog_dumi.sql
@@ -12,12 +12,12 @@ INSERT INTO ADRESS (adress_no, do, gu, dong, new_adress)
 VALUES (4, '수원시', '영통구', '이의동', '수원시 영통구 이의로');
 
 -- Care 테이블 - 보호소(최대3개만 등록)
-INSERT INTO CARE (care_no, adress_no, name, adress, tel, closeday, mans, week_open, week_close, week_adopt_str, week_adopt_end, weekend_open, weekend_close, weekend_adopt_str, weekend_adopt_end, map_url)
-VALUES (1, 3, '해피파워', '57-26', '010-1234-5678', '매주 목요일', 30, '09:00', '18:00', '10:00','17:00','09:00','20:00','10:00','19:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23401.192695856615!2d127.15998703550167!3d35.792659781764605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35702510baabe389%3A0x20f739be04cba361!2z656R656R7J2064SkIO2VmOyImQ!5e0!3m2!1sko!2skr!4v1693900715878!5m2!1sko!2skr');
-INSERT INTO CARE (care_no, adress_no, name, adress, tel, closeday, mans, week_open, week_close, week_adopt_str, week_adopt_end, weekend_open, weekend_close, weekend_adopt_str, weekend_adopt_end, map_url)
-VALUES (2, 2, '굿도그', '357 KR', '010-5678-9101', '매주 목요일', 40, '09:00', '18:00', '10:00','17:00','09:00','20:00','10:00','19:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4173.778456239814!2d127.30491264159748!3d37.33584054588896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356355f4a1f0641b%3A0x56ebac08078de44f!2z7JWE7J207KGw7JWEIOyalOyWkeuztO2YuOyGjCDqsr3quLDqtJHso7zsoJA!5e0!3m2!1sko!2skr!4v1693901340631!5m2!1sko!2skr');
-INSERT INTO CARE (care_no, adress_no, name, adress, tel, closeday, mans, week_open, week_close, week_adopt_str, week_adopt_end, weekend_open, weekend_close, weekend_adopt_str, weekend_adopt_end, map_url)
-VALUES (3, 1, '춘향이와멍멍이', '139나길 83', '010-9876-5432', '매주 목요일', 40, '09:00', '18:00', '10:00','17:00','09:00','20:00','10:00','19:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1348.064582504651!2d127.10170285603522!3d37.61888852972289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cba21c49379e7%3A0x18e763e51b5158de!2z7ISc7Jq47Jyg6riw64-Z66y87J6F7JaR7IS87YSw!5e0!3m2!1sko!2skr!4v1693901528111!5m2!1sko!2skr');
+INSERT INTO CARE (care_no, name, adress, tel, closeday, mans, open, close, volunteer_am, volunteer_pm, map_url)
+VALUES (1, '해피파워', '57-26', '010-1234-5678', '매주 목요일', 30, '09:00', '18:00', '9:00 ~ 13:00', '14:00 ~ 18:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23401.192695856615!2d127.15998703550167!3d35.792659781764605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35702510baabe389%3A0x20f739be04cba361!2z656R656R7J2064SkIO2VmOyImQ!5e0!3m2!1sko!2skr!4v1693900715878!5m2!1sko!2skr');
+INSERT INTO CARE (care_no, name, adress, tel, closeday, mans, open, close, volunteer_am, volunteer_pm, map_url)
+VALUES (2, '굿도그', '357 KR', '010-5678-9101', '매주 목요일', 40, '09:00', '18:00', '9:00 ~ 13:00', '14:00 ~ 18:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4173.778456239814!2d127.30491264159748!3d37.33584054588896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356355f4a1f0641b%3A0x56ebac08078de44f!2z7JWE7J207KGw7JWEIOyalOyWkeuztO2YuOyGjCDqsr3quLDqtJHso7zsoJA!5e0!3m2!1sko!2skr!4v1693901340631!5m2!1sko!2skr');
+INSERT INTO CARE (care_no, name, adress, tel, closeday, mans, open, close, volunteer_am, volunteer_pm, map_url)
+VALUES (3, '춘향이와멍멍이', '139나길 83', '010-9876-5432', '매주 목요일', 40, '09:00', '18:00', '9:00 ~ 13:00', '14:00 ~ 18:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1348.064582504651!2d127.10170285603522!3d37.61888852972289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cba21c49379e7%3A0x18e763e51b5158de!2z7ISc7Jq47Jyg6riw64-Z66y87J6F7JaR7IS87YSw!5e0!3m2!1sko!2skr!4v1693901528111!5m2!1sko!2skr');
 
 -- Care_admin 테이블 - 보호소 관리자
 INSERT INTO CARE_ADMIN (care_id, care_no, passwd)
--- workspace/src/test/java/tteogipbangbeomdae/goddog/DogMapperTest.java
+++ workspace/src/test/java/tteogipbangbeomdae/goddog/DogMapperTest.java
@@ -1,5 +1,7 @@
 package tteogipbangbeomdae.goddog;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,37 +9,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import lombok.extern.slf4j.Slf4j;
 import tteogipbangbeomdae.goddog.domain.dog.dto.Dog;
 import tteogipbangbeomdae.goddog.domain.dog.mapper.DogMapper;
+import tteogipbangbeomdae.goddog.domain.dog.service.DogService;
+import tteogipbangbeomdae.goddog.domain.openapi.service.OpenApiService;
 
 @SpringBootTest
 @Slf4j
 public class DogMapperTest {
 	
 	@Autowired
-	private DogMapper dogMapper;
+	private OpenApiService openApiService;
 	
 	@Test
 //	@Disabled
 	void createTest() {
-		Dog createDog = Dog
-				.builder()
-				.happenDt("230909")
-				.happenPlace("10")
-				.kindCd("10")
-//				.colorCd("10")
-				.age("10")
-				.weight("10")
-				.popfile("10")
-				.processState("10")
-				.sexCd("1")
-				.neuterYn("1")
-				.specialMark("10")
-				.careAddr("10")
-				.noticeComment("10")
-				.noticeSdt("20220104")
-				.noticeEdt("20220117")
-				.build();
-		dogMapper.create(createDog);
-		log.info("강아지 등록 완료 : {}", createDog);
+		List<Dog> dogs = openApiService.getDogList();
+		for (int i = 0; i < dogs.size(); i++) {
+			log.info("강아지 정보 : " + dogs.get(i));
+		}
 	}
 	
 }
