809ea6618d190f131a67a0723a74b9fc82138594
jaeheoon <aleh101@naver.com>
2023-10-05 23:39:04 +0900

    테스트

--- workspace/src/main/java/tteogipbangbeomdae/goddog/domain/common/web/dto/PageParams.java
+++ workspace/src/main/java/tteogipbangbeomdae/goddog/domain/common/web/dto/PageParams.java
@@ -23,10 +23,10 @@ import lombok.ToString;
 @AllArgsConstructor
 public class PageParams {
 	
-	private int elementSize;    /** 페이지에 보여지는 목록 갯수 */
-	private int pageSize;       /** 페이지에 보여지는 페이지 갯수 */
-	private int requestPage;    /** 사용자 요청 페이지 */
+	private int elementSize;    	/** 페이지에 보여지는 목록 갯수 */
+	private int pageSize;       	/** 페이지에 보여지는 페이지 갯수 */
+	private int requestPage;    	/** 사용자 요청 페이지 */
 	private String searchValue;     /** 사용자 검색값 */
-	private String searchType; /** 사용자 검색타입 */
-	private int rowCount;       /** 테이블 목록 갯수 */
+	private String searchType; 		/** 사용자 검색타입 */
+	private int rowCount;       	/** 테이블 목록 갯수 */
 }
